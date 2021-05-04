30d4b602fe40979114626f5e68b8ed0800ee10e4
Colin Kiama <colinkiama@gmail.com>
2021-05-04 02:27:00 +0100

    Site blocking in background script is working

--- src/js/background-script.js
+++ src/js/background-script.js
@@ -1,1 +1,27 @@
-console.log("Background page is alive!!!");
+import { getHostname } from "./utils/urlHelper.js";
+import BlockList from "./utils/blockList.js";
+
+
+
+// Need to filter with status property for Firefox versions older
+// than 88;
+const filter = {
+  properties: ["url", "status"],
+  windowId: window.browser.windows.WINDOW_ID_CURRENT
+}
+
+async function handleUpdated(tabId, changeInfo, tabInfo) {
+	const blockList = new BlockList();
+	let updatedTabHostname = getHostname(tabInfo.url);
+
+	let isInBlockList = await blockList.contains(updatedTabHostname);
+
+	if (isInBlockList) {
+		window.browser.tabs.update(tabId,{
+		url: window.browser.runtime.getURL("views/site-blocked.html"),
+		loadReplace: true
+		});
+	}
+}
+
+window.browser.tabs.onUpdated.addListener(handleUpdated, filter);
